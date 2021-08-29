3a448f57b019adf26154dc08d0bd50c91373868d
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-08-29 10:34:07 +0600

    Add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 <body class="bg-gray-100 h-screen antialiased leading-none font-sans">
     <div id="app">
-        <header class="bg-blue-900 py-6">
+        <header class="bg-gray-800 py-6">
             <div class="container mx-auto flex justify-between items-center px-6">
                 <div>
                     <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
@@ -25,6 +25,8 @@
                     </a>
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                    <a class="no-underline hover:underline" href="/">Home</a>
+                    <a class="no-underline hover:underline" href="/blog">Blog</a>
                     @guest
                         <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
                         @if (Route::has('register'))
@@ -45,7 +47,13 @@
             </div>
         </header>
 
-        @yield('content')
+        <div>
+            @yield('content')
+        </div>
+
+        <div>
+            @include('layouts.footer')
+        </div>
     </div>
 </body>
 </html>
