1566dbdebbd68d289c722b65fdb28d414a7528b1
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-08-29 14:43:53 +0600

    Show blog posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostsController extends Controller
 {
@@ -13,7 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('blog.index');
+        return view('blog.index')
+            ->with('posts', Post::orderBy('updated_at', 'DESC')->get());
     }
 
     /**
