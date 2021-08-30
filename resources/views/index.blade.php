61263a5eca0ad31b930d00a3bac73be2907f40e1
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-08-30 21:40:15 +0600

    Modify homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,13 +4,13 @@
     <div class="background-image grid grid-cols-1 m-auto">
         <div class="flex text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
-                <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                    Do you want to become a developer?
+                <h1 class="sm:text-gray-700 text-5xl uppercase font-bold text-shadow-md pb-14">
+                    Want to know how to become a better human?
                 </h1>
                 <a 
                     href="/blog"
                     class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
-                    Read More
+                    Let's Find Out
                 </a>
             </div>
         </div>
@@ -22,46 +22,46 @@
         </div>
 
         <div class="m-auto sm:m-auto text-left w-4/5 block">
-            <h2 class="text-3xl font-extrabold text-gray-600">
-                Struggling to be a better web developer?
+            <h2 class="text-2xl font-bold text-gray-600">
+                Struggling to be a better human?
             </h2>
-            
-            <p class="py-8 text-gray-500 text-s">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus.
-            </p>
+     
 
-            <p class="font-extrabold text-gray-600 text-s pb-9">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam vero nostrum! Perferendis eos molestias porro vero. Vel alias.
+            <p class="py-8 text-gray-600 text-s pb-9">
+            It's harder to be your best self when another person is giving orders. At the point when you're holding the reins, you're enabled to settle on the decisions and choices that guarantee your best self flourishes.
             </p>
 
             <a 
                 href="/blog"
-                class="uppercase bg-blue-500 text-gray-100 text-s font-extrabold py-3 px-8 rounded-3xl">
+                class="uppercase bg-blue-500 text-gray-100 text-s font-bold py-3 px-8 rounded-3xl">
                 Find Out More
             </a>
         </div>
     </div>
 
-    <div class="text-center p-15 bg-black text-white">
+    <div class="text-center p-15 bg-gray-800 text-green-100">
         <h2 class="text-2xl pb-5 text-l"> 
-            I'm an expert in...
+        The 5 Most Important Things in Life
         </h2>
 
-        <span class="font-extrabold block text-4xl py-1">
-            Ux Design
+        <span class="font-bold block text-3xl py-3">
+        Family
         </span>
-        <span class="font-extrabold block text-4xl py-1">
-            Project Management
+        <span class="font-bold block text-3xl py-3">
+        Friends
         </span>
-        <span class="font-extrabold block text-4xl py-1">
-            Digital Strategy
+        <span class="font-bold block text-3xl py-3">
+        Love
         </span>
-        <span class="font-extrabold block text-4xl py-1">
-            Backend Development
+        <span class="font-bold block text-3xl py-3">
+        Dream
+        </span>
+        <span class="font-bold block text-3xl py-3">
+        Health
         </span>
     </div>
 
-    <div class="text-center py-15">
+    <!-- <div class="text-center py-15">
         <span class="uppercase text-s text-gray-400">
             Blog
         </span>
@@ -73,23 +73,23 @@
         <p class="m-auto w-4/5 text-gray-500">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
         </p>
-    </div>
+    </div> -->
 
-    <div class="sm:grid grid-cols-2 w-4/5 m-auto">
+    <div class="sm:grid grid-cols-2 w-4/5 m-auto py-15 mt-10">
         <div class="flex bg-yellow-700 text-gray-100 pt-10">
             <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
                 <span class="uppercase text-xs">
-                    PHP
+                   Happiness
                 </span>
 
                 <h3 class="text-xl font-bold py-10">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
+                Happiness is not the absence of problems, it’s the ability to deal with them. -- Steve Maraboli
                 </h3>
 
                 <a 
-                    href=""
+                    href="/blog"
                     class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-                    Find Out More
+                    Read More
                 </a>
             </div>
         </div>
